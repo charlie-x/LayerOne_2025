e455d779e3b2da41291d09bebcd01915da7b505c
charlie <charlie.wallace@otoy.com>
2025-05-24 16:24:14 -0700

    added scripts, not fully implemented, delay and gpio pins works

---
+++ scripts/glitch.sh
@@ -0,0 +1,18 @@
+glitch duration 100
+glitch trigger
+glitch status
+glitch duration 5000
+sleep_ms 1000
+g
+glitch duration 20000
+sleep_ms 1000
+g
+glitch duration 30000
+sleep_ms 1000
+g
+glitch duration 40000
+sleep_ms 1000
+g
+glitch duration 50000
+sleep_ms 1000
+g
